b14d92f73b33dab5420f550fa8025830c0947bd7
rizza <denostarizza43@gmail.com>
2023-04-27 16:54:31 +1200

    numbers.txt file opened

--- text_file.py
+++ text_file.py
@@ -1,8 +1,8 @@
 # Open the numbers.txt file for reading
-# Read the contents of the file into a list of integers
+with open('numbers.txt', 'r') as file:
+    # Read the contents of the file into a list of integers
 # Create two new files, even.txt and odd.txt, for writing
 # Loop through each number in the list
 # Check if the number is even
 # Write the even number to the even.txt file
-# Write the odd number to the odd.txt file
-
+# # Write the odd number to the odd.txt file
