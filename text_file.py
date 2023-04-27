7213bc5a13143bb73b4a6bfb84c0b8a8ecdafc42
rizza <denostarizza43@gmail.com>
2023-04-27 17:18:27 +1200

    for loop

--- text_file.py
+++ text_file.py
@@ -5,7 +5,8 @@ with open('numbers.txt', 'r') as file:
 
 # Create two new files, even.txt and odd.txt, for writing
 with open('even.txt', 'w') as even_file, open('odd.txt', 'w') as odd_file:
-# Loop through each number in the list
+    # Loop through each number in the list
+    for number in numbers:
 # Check if the number is even
 # Write the even number to the even.txt file
-# # Write the odd number to the odd.txt file
+# Write the odd number to the odd.txt file
