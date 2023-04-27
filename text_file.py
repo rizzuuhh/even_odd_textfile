957e9145a0a84121663cb1daa4cfcb99659c8812
rizza <denostarizza43@gmail.com>
2023-04-27 17:58:12 +1200

    yey odd numbers were now writen in the odd.txt file

--- text_file.py
+++ text_file.py
@@ -3,13 +3,16 @@ with open('numbers.txt', 'r') as file:
     # Read the contents of the file into a list of integers
     numbers = [int(number) for number in file.read().split()]
 
+
 # Create two new files, even.txt and odd.txt, for writing
 with open('even.txt', 'w') as even_file, open('odd.txt', 'w') as odd_file:
     # Loop through each number in the list
     for number in numbers:
         # Check if the number is even
-         if number % 2 == 0:
+        if number % 2 == 0:
             # Write the even number to the even.txt file
-            even_file.write(str(number) + '\n')
-
-# Write the odd number to the odd.txt file 
+            even_file.write(str(number) + '\n')   
+        else:
+            # Write the odd number to the odd.txt file
+            odd_file.write(str(number) + '\n')
+           
