66c2c3affae621f73773d0bb14eb48e7bac18b0d
rizza <denostarizza43@gmail.com>
2023-04-27 17:04:31 +1200

    listed 20 integers in numbers.txt file

--- text_file.py
+++ text_file.py
@@ -1,6 +1,8 @@
 # Open the numbers.txt file for reading
 with open('numbers.txt', 'r') as file:
     # Read the contents of the file into a list of integers
+    numbers = [int(number) for number in file.read().split()]
+
 # Create two new files, even.txt and odd.txt, for writing
 # Loop through each number in the list
 # Check if the number is even
